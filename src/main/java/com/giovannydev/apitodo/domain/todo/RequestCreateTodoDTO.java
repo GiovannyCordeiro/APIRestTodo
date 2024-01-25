bcac5e863afb58bf7b124c0af3f4e2d8af240e1f
Giovanny Cordeiro <Giovannycordeiropb@gmail.com>
2024-01-25 10:55:36 -0300

    feat: Adding request create DTO

---
+++ src/main/java/com/giovannydev/apitodo/domain/todo/RequestCreateTodoDTO.java
@@ -0,0 +1,14 @@
+package com.giovannydev.apitodo.domain.todo;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record RequestCreateTodoDTO (
+  @NotBlank
+  String name,
+  String description,
+  String category,
+  @NotNull
+  Boolean status
+){
+}
