6683215dfe841c74c54d4c257339a23e0756090a
Giovanny Cordeiro <Giovannycordeiropb@gmail.com>
2024-01-26 17:07:36 -0300

    feat: adding put verb in controller

--- src/main/java/com/giovannydev/apitodo/controller/TodosController.java
+++ src/main/java/com/giovannydev/apitodo/controller/TodosController.java
@@ -6,12 +6,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.giovannydev.apitodo.domain.todo.RequestCreateTodoDTO;
 import com.giovannydev.apitodo.domain.todo.Todos;
+import com.giovannydev.apitodo.domain.todo.DTOs.RequestUpdateDTO;
 import com.giovannydev.apitodo.services.todo.CreateTodoService;
 import com.giovannydev.apitodo.services.todo.GetTodosService;
 
@@ -35,5 +37,10 @@ public class TodosController {
   @PostMapping
   public ResponseEntity<Todos> createTodo(@RequestBody @Validated RequestCreateTodoDTO data){
     return createTodo.createTodo(data);
+  }
+
+  @PutMapping
+  public ResponseEntity updateTodo(@Validated @RequestBody RequestUpdateDTO data){
+    return ResponseEntity.ok().build();
   }
 }
