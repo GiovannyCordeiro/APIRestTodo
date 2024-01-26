99106abbddff2d6dc389e85d583e4bbeb666f1bd
Giovanny Cordeiro <Giovannycordeiropb@gmail.com>
2024-01-26 17:07:18 -0300

    feat: DTO for update

---
+++ src/main/java/com/giovannydev/apitodo/domain/todo/DTOs/RequestUpdateDTO.java
@@ -0,0 +1,12 @@
+package com.giovannydev.apitodo.domain.todo.DTOs;
+
+import jakarta.validation.constraints.NotNull;
+
+public record RequestUpdateDTO(
+  @NotNull
+  Integer id,
+  String name,
+  String description,
+  String category,
+  Boolean status
+) {}
